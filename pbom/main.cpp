65f865cee937ed840204ca644345f28556998f9e
Nikita Kobzev <win.accuracy@gmail.com>
2021-09-16 19:47:37 +0300

    Working on the CLI options

--- pbom/main.cpp
+++ pbom/main.cpp
@@ -7,7 +7,8 @@
 #include "util/exception.h"
 #include "util/log.h"
 #include <QScopedPointer>
-#include <QThread>
+#include "model/pbomodel.h"
+#include <CLI/CLI.hpp>
 
 #define LOG(...) LOGGER("Main", __VA_ARGS__)
 
@@ -27,51 +28,111 @@ namespace pboman3 {
             }
         }
     };
+
+    void ActivateCom(const QApplication& a) {
+        auto* timer = new QTimer(new QTimer());
+        timer->moveToThread(a.thread());
+        timer->setSingleShot(true);
+        QObject::connect(timer, &QTimer::timeout, [timer]() {
+            LOG(info, "Initializing COM")
+
+            const HRESULT hr = CoInitialize(nullptr);
+
+            LOG(info, "COM init status:", hr)
+
+            assert(SUCCEEDED(hr));
+            timer->deleteLater();
+        });
+        timer->start();
+    }
+
+    int RunMainWindow(const QApplication& app, const std::string& pboFile) {
+        using namespace pboman3;
+
+        ACTIVATE_ASYNC_LOG_SINK
+
+        LOG(info, "Starting the app")
+
+        ActivateCom(app);
+
+        LOG(info, "Display the main window")
+        const auto model = QScopedPointer(new PboModel());
+        MainWindow w(nullptr, model.get());
+        w.show();
+
+        if (!pboFile.empty()) {
+            const QString file(pboFile.c_str());
+            LOG(info, "Loading the file:", file)
+            w.loadFile(file);
+        }
+
+        const int exitCode = app.exec();
+
+        LOG(info, "The app exiting with the code:", exitCode)
+
+        return exitCode;
+    }
+
+
+    int RunPackWindow(const QApplication& app, const std::vector<std::string>& folders, const std::string& outDir) {
+        qInfo() << "Pack";
+        return 0;
+    }
+
+    int RunUnpackWindow(const QApplication& app, const std::vector<std::string>& files, const std::string& outDir) {
+        qInfo() << "Unpack";
+        return 0;
+    }
 }
 
 int main(int argc, char* argv[]) {
+    using namespace CLI;
     using namespace pboman3;
 
-    PboApplication a(argc, argv);
-    ACTIVATE_ASYNC_LOG_SINK
+    App cli("PBO Manager V3");
+    cli.require_subcommand(0, 1);
 
-    LOG(info, "Starting the app")
+    App* cmdOpen = cli.add_subcommand("open", "Open the specified PBO file");
+    std::string cmdOpenFilePath;
+    auto cmdOpenFile = cmdOpen->add_option("file", cmdOpenFilePath, "The PBO file to open")
+                              ->required()
+                              ->check(ExistingFile);
 
-    auto* timer = new QTimer(new QTimer());
-    timer->moveToThread(a.thread());
-    timer->setSingleShot(true);
-    QObject::connect(timer, &QTimer::timeout, [timer]() {
-        LOG(info, "Initializing COM")
+    App* cmdPack = cli.add_subcommand("pack", "Pack the specified folders(s) as PBO(s)");
+    std::vector<std::string> cmdPackFoldersPath;
+    Option* cmdPackFolders = cmdPack->add_option("folders", cmdPackFoldersPath, "The folder(s) to pack")
+                                    ->required()
+                                    ->check(ExistingDirectory);
+    std::string cmdPackOutPath;
+    Option* cmdPackOut = cmdPack->add_option("-o,--output-directory", cmdPackOutPath,
+                                             "The directory to write the resulting PBO(s)")
+                                ->check(ExistingDirectory);
 
-        const HRESULT hr = CoInitialize(nullptr);
+    App* cmdUnpack = cli.add_subcommand("unpack", "Unpack the specified PBO(s)");
+    std::vector<std::string> cmdUnpackFilesPath;
+    Option* cmdUnpackFiles = cmdUnpack->add_option("files", cmdUnpackFilesPath, "The PBO(s) to unpack")
+        ->required()
+        ->check(ExistingFile);
+    std::string cmdUnpackOutPath;
+    Option* cmdUnpackOut = cmdUnpack->add_option("-o,--output-directory", "The directory to write the PBO(s) contents")
+                                    ->check(ExistingDirectory);
 
-        LOG(info, "COM init status:", hr)
+    CLI11_PARSE(cli, argc, argv)
 
-        assert(SUCCEEDED(hr));
-        timer->deleteLater();
-    });
-    timer->start();
-
-    LOG(info, "Display the main window")
-    const auto model = QScopedPointer(new PboModel());
-    MainWindow w(nullptr, model.get());
-    w.show();
-
-    LOG(info, "Number of startup args:", argc)
-
-    if (argc > 1) {
-        const QString file(argv[1]);
-        if (QFile::exists(file)) {
-            LOG(info, "Loading the file:", file);
-            w.loadFile(file);
-        } else {
-            LOG(info, "The arg file did not exist:", file);
-        }
+    int exitCode;
+    if (cli.got_subcommand(cmdOpen)) {
+        const PboApplication app(argc, argv);
+        exitCode = RunMainWindow(app, cmdOpenFilePath);
+    } else if (cli.got_subcommand(cmdPack)) {
+        const PboApplication app(argc, argv);
+        exitCode = RunPackWindow(app, std::vector<std::string>(), "");
+    } else if (cli.got_subcommand(cmdUnpack)) {
+        const PboApplication app(argc, argv);
+        exitCode = RunUnpackWindow(app, std::vector<std::string>(), "");
+    } else {
+        const PboApplication app(argc, argv);
+        exitCode = RunMainWindow(app, "");
     }
-
-    const int exitCode = a.exec();
-
-    LOG(info, "The app exiting with the code:", exitCode);
 
     return exitCode;
 }

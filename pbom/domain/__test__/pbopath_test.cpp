6cd13be27ebac8fd245d12e22c2810a29cb9461c
Nikita Kobzev <win.accuracy@gmail.com>
2023-01-15 22:29:02 +0300

    Teach the app to deal with losts of / and \ symbols in the pbo entry path

--- pbom/domain/__test__/pbopath_test.cpp
+++ pbom/domain/__test__/pbopath_test.cpp
@@ -32,6 +32,38 @@ namespace pboman3::domain::test {
         ASSERT_EQ(p2.at(2), "e3");
     }
 
+    TEST(PboPath, Ctor_Handles_Paths_With_Multiple_Slashes) {
+        const PboPath p1("////");
+        ASSERT_EQ(p1.length(), 1);
+        ASSERT_EQ(p1.at(0), "////");
+
+        const PboPath p2("////a/");
+        ASSERT_EQ(p2.length(), 1);
+        ASSERT_EQ(p2.at(0), "////a/");
+
+        const PboPath p3("/a/");
+        ASSERT_EQ(p3.length(), 1);
+        ASSERT_EQ(p3.at(0), "/a/");
+
+        const PboPath p4("//a//b");
+        ASSERT_EQ(p4.length(), 2);
+        ASSERT_EQ(p4.at(0), "//a");
+        ASSERT_EQ(p4.at(1), "/b");
+
+        const PboPath p5("\\\\");
+        ASSERT_EQ(p5.length(), 1);
+        ASSERT_EQ(p5.at(0), "\\\\");
+
+        const PboPath p6("\\a\\");
+        ASSERT_EQ(p6.length(), 1);
+        ASSERT_EQ(p6.at(0), "\\a\\");
+
+        const PboPath p7("\\a\\b");
+        ASSERT_EQ(p7.length(), 2);
+        ASSERT_EQ(p7.at(0), "\\a");
+        ASSERT_EQ(p7.at(1), "b");
+    }
+
     TEST(PboPath, MakeSibling_Creates_Sibling) {
         const PboPath p1("e1/f1");
         const PboPath s1 = p1.makeSibling("f2");

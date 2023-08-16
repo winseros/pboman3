63213ee6ac5d5fc1fa905efcfb72d70a7f7a8e8c
Nikita Kobzev <win.accuracy@gmail.com>
2023-08-16 22:07:02 +0300

    The console app now also handles russian
    Update the command line parser

--- pbom/console.cpp
+++ pbom/console.cpp
@@ -32,43 +32,45 @@ namespace pboman3 {
         return 0;
     }
 
-    int RunWithCliOptions(int argc, char* argv[]) {
+    template <typename TChr>
+    int RunWithCliOptions(int argc, TChr* argv[]) {
         using namespace CLI;
         using namespace pboman3;
 
         int exitCode;
         App cli;
-            const CommandLine cmd(&cli);
-            const shared_ptr<CommandLine::Result> commandLine = cmd.build();
-            CLI11_PARSE(cli, argc, argv)
+        const CommandLine cmd(&cli);
+        const shared_ptr<CommandLine::Result<TChr>> commandLine = cmd.build<TChr>();
+        CLI11_PARSE(cli, argc, argv)
 
-            if (commandLine->pack.hasBeenSet()) {
-                QString outputDir;
-                if (commandLine->pack.hasOutputPath())
-                    outputDir = CommandLine::toQt(commandLine->pack.outputPath);
-                else
-                    outputDir = QDir::currentPath();
+        if (commandLine->pack.hasBeenSet()) {
+            QString outputDir;
+            if (commandLine->pack.hasOutputPath())
+                outputDir = CommandLine::toQt(commandLine->pack.outputPath);
+            else
+                outputDir = QDir::currentPath();
 
-                const QStringList folders = CommandLine::toQt(commandLine->pack.folders);
-                exitCode = RunConsolePackOperation(folders, outputDir);
-            } else if (commandLine->unpack.hasBeenSet()) {
-                QString outputDir;
-                if (commandLine->unpack.hasOutputPath())
-                    outputDir = CommandLine::toQt(commandLine->unpack.outputPath);
-                else
-                    outputDir = QDir::currentPath();
+            const QStringList folders = CommandLine::toQt(commandLine->pack.folders);
+            exitCode = RunConsolePackOperation(folders, outputDir);
+        } else if (commandLine->unpack.hasBeenSet()) {
+            QString outputDir;
+            if (commandLine->unpack.hasOutputPath())
+                outputDir = CommandLine::toQt(commandLine->unpack.outputPath);
+            else
+                outputDir = QDir::currentPath();
 
-                const QStringList files = CommandLine::toQt(commandLine->unpack.files);
-                exitCode = RunConsoleUnpackOperation(files, outputDir);   
-            } else {
-                //should not normally get here; if did - CLI11 was misconfigured somewhere
-                cout << cli.help();
-                exitCode = 1;
-            }
+            const QStringList files = CommandLine::toQt(commandLine->unpack.files);
+            exitCode = RunConsoleUnpackOperation(files, outputDir);
+        } else {
+            //should not normally get here; if did - CLI11 was misconfigured somewhere
+            cout << cli.help();
+            exitCode = 1;
+        }
         return exitCode;
     }
 
-    int RunMain(int argc, char* argv[]) {
+    template <typename TChr>
+    int RunMain(int argc, TChr* argv[]) {
         const int exitCode = RunWithCliOptions(argc, argv);
         return exitCode;
     }
@@ -81,7 +83,11 @@ void HandleEptr(const std::exception_ptr& ptr) try {
     LOG(critical, "Uncaught exception has been thrown:", ex.what())
 }
 
+#ifdef WIN32
+int wmain(int argc, wchar_t* argv[]) {
+#else
 int main(int argc, char* argv[]) {
+#endif
     try {
         const int res = pboman3::RunMain(argc, argv);
         return res;

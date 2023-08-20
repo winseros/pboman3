2786ca8c142ca965368859472dbae9dd6578bfd5
Nikita Kobzev <win.accuracy@gmail.com>
2023-08-20 13:50:34 +0300

    Now using 16 bit path when opening files

--- pbom/ui/win32/win32fileviewer.h
+++ pbom/ui/win32/win32fileviewer.h
@@ -6,9 +6,27 @@
 namespace pboman3::ui {
     class Win32FileViewerException : public AppException {
     public:
-        Win32FileViewerException(QString message);
+        Win32FileViewerException(QString message, unsigned long systemErrorCode, QString systemErrorDescription,
+                                 QString filePath);
 
         PBOMAN_EX_HEADER(Win32FileViewerException)
+
+        [[nodiscard]] unsigned long systemErrorCode() const {
+            return systemErrorCode_;
+        }
+
+        [[nodiscard]] QString systemErrorDescription() const {
+            return systemErrorDescription_;
+        }
+
+        [[nodiscard]] QString filePath() const {
+            return filePath_;
+        }
+
+    private:
+        unsigned long systemErrorCode_;
+        QString systemErrorDescription_;
+        QString filePath_;
     };
 
     class Win32FileViewer : public FileViewer {
